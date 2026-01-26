f8f51028f0ececfdf320cde7985053906fb1522e
QuylV <quylv@slus-sofewave.com.vn>
2026-01-26 15:06:28 +0700

    CRUD API functionality fastAPI Python

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
+from models import (supplier_pydantic, supplier_pydanticIn, Supplier)
 
 app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"Hello": "World"}
+    return {"Msg": "go to /docs for api documentation"}
+
+@app.post("/supplier/")
+async def add_supplier(supplier_info: supplier_pydanticIn):
+    supplier_obj = await Supplier.create(**supplier_info.dict(exclude_unset=True))
+    repsonse = await supplier_pydantic.from_tortoise_orm(supplier_obj)
+    return {"status": "success", "data": repsonse}
+
+@app.get("/suppliers")
+async def get_all_suppliers():
+    suppliers = await supplier_pydantic.from_queryset(Supplier.all())
+    return {"status": "success", "data": suppliers}
+
+@app.get("/supplier/{supplier_id}")
+async def get_supplier(supplier_id: int):
+    supplier = await supplier_pydantic.from_queryset_single(Supplier.get(id=supplier_id))
+    return {"status": "success", "data": supplier}
+
+@app.put("/supplier/{supplier_id}")
+async def update_supplier(supplier_id: int, supplier_info: supplier_pydanticIn):
+    await Supplier.filter(id=supplier_id).update(**supplier_info.dict(exclude_unset=True))
+    supplier = await supplier_pydantic.from_queryset_single(Supplier.get(id=supplier_id))
+    return {"status": "success", "data": supplier}
+
+@app.delete("/supplier/{supplier_id}")
+async def delete_supplier(supplier_id: int):
+    deleted_count = await Supplier.filter(id=supplier_id).delete()
+    if not deleted_count:
+        return {"status": "error", "message": f"Supplier with id {supplier_id} not found"}
+    return {"status": "success", "message": f"Deleted supplier with id {supplier_id}"}
 
 register_tortoise(
     app,
